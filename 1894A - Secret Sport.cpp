1e182ada50b1a2f306ae511d4962251adc171cd7
SOMBIT MAZUMDER <115360765+SOMBIT4@users.noreply.github.com>
2024-03-24 19:19:32 +0600

    Add files via upload

---
+++ 1894A - Secret Sport.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main ()
+{
+    int n,t;
+    string s;
+    cin >> t;
+    while(t--){
+    	cin >> n;
+    	cin >> s;
+    	cout << s[n-1] <<endl;
+    }  
+  return 0;
+}
